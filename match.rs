68ded0670730bc795918731f5008702838e90820
amogh-2 <amoghbhattarai3205@gmail.com>
2025-07-06 11:11:01 +0545

    to add match

---
+++ match.rs
@@ -0,0 +1,22 @@
+fn main(){
+    get_option(3);
+}    
+
+fn get_option(tp:u8){
+    match tp{
+        1   => {
+            println!("Execute case 1")
+        }
+        2   => {
+            println!("Execute case 2")
+        }
+        _   => {
+            never_return();
+        }
+    };
+    
+}
+
+fn never_return()->!{
+    panic!()
+}
