74fd3ef0b94dd38c016a02f6b6d69062fda5d4dc
amogh-2 <amoghbhattarai3205@gmail.com>
2025-07-07 19:06:16 +0545

    to add input

--- input.rs
+++ input.rs
@@ -1,6 +1,13 @@
 use std::io;
 
 fn main(){
-    let mut 
+    println!("Enter a string: ");
+    let mut input = String::new();
+    
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    println!("The output is: {}", input);
     
 }
