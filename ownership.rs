2912817f583fe231b649d90b8066e94efa15077b
amogh-2 <amoghbhattarai3205@gmail.com>
2025-07-08 12:20:44 +0545

    to add ownership.rs

---
+++ ownership.rs
@@ -0,0 +1,17 @@
+fn main(){
+
+    let s=String::from("Hello World!"); //string is allocated in heap memory
+    take_ownership(s);
+    //println!("{}",s) this is not possible as the owner of the string isn't s anymore after the above line but is the function take_ownership()
+    let x=5;    //integer is allocated in stack memory
+    makes_copy(x); 
+    println!("Printing integer from main: {}", x) //this is possible as the ownership of the integer 5 is just copied to the makes_copy fn and not moved
+}
+
+fn take_ownership(a_string:String){
+    println!("{}", a_string);
+}
+
+fn makes_copy(an_integer:i32){
+    println!("Printing integer from makes_copy fn: {}",an_integer); 
+}
