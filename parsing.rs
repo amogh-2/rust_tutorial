90f5d23fe26e748f8fee1154b69750c11ffe6eeb
amogh-2 <amoghbhattarai3205@gmail.com>
2025-07-08 09:00:04 +0545

    to add parsing a string to int

---
+++ parsing.rs
@@ -0,0 +1,15 @@
+use std::io;
+fn main(){
+    println!("Enter a number: ");
+    let mut x= String::new();
+    
+    io::stdin()
+        .read_line(&mut x)
+        .expect("Failed to read line");
+
+    let y=x.trim().parse::<i32>().expect("Couldn't convert");
+
+    println!("The next number is {}",y+1);
+
+
+}
