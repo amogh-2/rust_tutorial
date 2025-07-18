2a65e442908cb43195affe6d7aa9c532c41232ab
amogh-2 <amoghbhattarai3205@gmail.com>
2025-07-18 17:44:36 +0545

    vector string are like arrays but mutable

---
+++ vector_string.rs
@@ -0,0 +1,7 @@
+fn main(){
+    let mut x:Vec<String>= Vec::new();
+    x.push("String_1".to_string());
+    x.push("String_2".to_string());
+    x.push("String_3".to_string());
+    println!("{}",x[1]);
+}
