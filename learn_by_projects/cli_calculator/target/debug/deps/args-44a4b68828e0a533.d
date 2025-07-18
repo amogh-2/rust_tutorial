2e6e234b2040da80d1ae256547a56f6a2397f18f
amogh-2 <amoghbhattarai3205@gmail.com>
2025-07-18 16:52:47 +0545

    cli arguments for calculator

---
+++ learn_by_projects/cli_calculator/target/debug/deps/args-44a4b68828e0a533.d
@@ -0,0 +1,5 @@
+/home/abey/learnings/rust/learn_by_projects/cli_calculator/target/debug/deps/args-44a4b68828e0a533.d: src/main.rs
+
+/home/abey/learnings/rust/learn_by_projects/cli_calculator/target/debug/deps/args-44a4b68828e0a533: src/main.rs
+
+src/main.rs:
