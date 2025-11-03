60f2d4e9230fafb8603931cccf911693f96ee298
amogh <amoghbhattarai3205@gmail.com>
2025-11-04 00:43:32 +0545

    still learning match

---
+++ learn_by_projects/hack_tool/src/main.rs
@@ -0,0 +1,43 @@
+use std::io;
+
+fn main() {
+    println!("\n\n\nYou are an underground hacker attempting to infiltrate a classified AI network.\nYou start at Trace Level 0 — if your trace level hits 3, you get caught. \nYour goal is to reach the Core Node by passing through all 3 security layers.\n\nPress Enter to continue or q to quit. ");
+    let mut enter_press= String::new();
+    io::stdin().read_line(&mut enter_press).expect("Error taking input");
+    let enter_press=enter_press.trim();
+    if enter_press.is_empty(){
+        start_game();
+    }
+    else{
+        println!("See you next time then Agent, Adios...");
+    }
+}
+
+fn start_game(){
+    println!("Initializing secure terminal...\nConnecting to network...\nAccessing mainframe [OK].\nWelcome, Agent 0x75B.");
+    loop{
+        println!("Press: ");
+        println!("1. To Scan Network\n2. To Attack Node\n3. To View Status\n4. Exit");
+        let mut press= String::new();
+        io::stdin().read_line(&mut press).expect("Error taking input");
+        let press=press.trim().parse::<i32>().expect("Invalid number");
+        match press{
+            1 => {
+                    println!("Execute situation 1");
+            }
+            2 => {
+                    println!("Execute situation 2");
+            }
+            3 => { 
+                    println!("Execute situation 3");
+            }
+            4 => {
+                    println!("Exiting...");
+                    break;
+            }
+            _ =>{
+                println!("Invalid Input");
+            }
+        }
+    }
+}
---
+++ learn_by_projects/hack_tool/target/debug/deps/hack_tool-f31addceea151609.d
@@ -0,0 +1,5 @@
+C:\Users\amogh\Desktop\rust\rust_tutorial\learn_by_projects\hack_tool\target\debug\deps\hack_tool-f31addceea151609.d: src\main.rs
+
+C:\Users\amogh\Desktop\rust\rust_tutorial\learn_by_projects\hack_tool\target\debug\deps\hack_tool-f31addceea151609.exe: src\main.rs
+
+src\main.rs:
