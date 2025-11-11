953ea24c4447b6a3b29d30480387d3064ae5accf
amogh <amoghbhattarai3205@gmail.com>
2025-11-11 23:01:30 +0545

    match_case

--- learn_by_projects/hack_tool/src/main.rs
+++ learn_by_projects/hack_tool/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     println!("\n\n\nYou are an underground hacker attempting to infiltrate a classified AI network.\nYou start at Trace Level 0 — if your trace level hits 3, you get caught. \nYour goal is to reach the Core Node by passing through all 3 security layers.\n\nPress Enter to continue or q to quit. ");
@@ -16,7 +18,7 @@ fn main() {
 fn start_game(){
     println!("Initializing secure terminal...\nConnecting to network...\nAccessing mainframe [OK].\nWelcome, Agent 0x75B.");
     loop{
-        println!("Press: ");
+        println!("\nPress: ");
         println!("1. To Scan Network\n2. To Attack Node\n3. To View Status\n4. Exit");
         let mut press= String::new();
         io::stdin().read_line(&mut press).expect("Error taking input");
@@ -24,15 +26,19 @@ fn start_game(){
         match press{
             1 => {
                     println!("Execute situation 1");
+                    thread::sleep(Duration::from_secs(2));
             }
             2 => {
                     println!("Execute situation 2");
+                    thread::sleep(Duration::from_secs(2));
             }
             3 => { 
                     println!("Execute situation 3");
+                    thread::sleep(Duration::from_secs(2));
             }
             4 => {
                     println!("Exiting...");
+                    thread::sleep(Duration::from_secs(2));
                     break;
             }
             _ =>{
