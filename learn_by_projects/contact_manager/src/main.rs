70c391384ef580fb591e57c1f91d6770defa4de3
amogh <amoghbhattarai3205@gmail.com>
2026-01-29 20:11:45 +0545

    this is a mess

---
+++ learn_by_projects/contact_manager/src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+struct ContactDetails{
+    name: String,
+    number: String,
+    email: String,
+}
+
+struct ContactManager{
+    contacts: Vec<ContactDetails>,
+}
+
+impl ContactDetails{
+    fn new(contacts: Vec<ContactDetails>)-> self{
+        ContactManager{contacts: vec::new()}
+    }
+    fn display(&self){
+        println!("The number is: ",self.name);
+    }
+    fn 
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
