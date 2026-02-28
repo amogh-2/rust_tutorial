6968df9f13017b1e2cc4dc9232b6f4bd8165900d
amogh <amoghbhattarai3205@gmail.com>
2026-02-28 23:45:16 +0545

    ceaser cipher

---
+++ learn_by_projects/ceaser_cipher/src/lib.rs
@@ -0,0 +1,5 @@
+/*This code defines two functions: encrypt and decrypt
+The encrypt function makes a plaintext string and a shift value, and returns the ciphertext string
+and returns the plain text string.
+*/
+
