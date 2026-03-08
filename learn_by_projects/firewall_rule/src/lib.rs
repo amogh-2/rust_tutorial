9aaedc8b75087d7b2b0af021b725c84d5e401cb6
amogh <amoghbhattarai3205@gmail.com>
2026-03-09 00:33:53 +0545

    firewall to add

--- learn_by_projects/firewall_rule/src/lib.rs
+++ learn_by_projects/firewall_rule/src/lib.rs
@@ -1,24 +1,24 @@
-struct Packet{
-    src_ip: String,
-    dst_ip: String,
-    port: u16,
+pub struct Packet{
+    pub src_ip: String,
+    pub dst_ip: String,
+    pub port: u16,
 }
 
-struct FireWallRule{
+pub struct FireWallRule{
     ip:String,
     port:u16,
     allow:bool,
 }
 
 impl FireWallRule{
-    pub fn new(ip: &str, port:&u16. allow:bool)->Self{
+    pub fn new(ip: &str, port: u16, allow:bool)->Self{
         Self{
             ip: ip.to_string(),
             port,
             allow,
         }
     }
-    pub fn matches(&self, packet: &Packet)-> bool{
+    fn matches(&self, packet: &Packet)-> bool{
         self.ip == packet.src_ip && self.port == packet.port
     }
     pub fn is_allowed(&self, packet: &Packet)-> bool{
@@ -29,8 +29,8 @@ impl FireWallRule{
             true
         }
     }
-    fn display(&self){
-        let statuts = if self.allow{"ALLOW"} else {|"BLOCK"};
+    pub fn display(&self){
+        let status = if self.allow{"ALLOW"} else {"BLOCK"};
         println!("{}:{} -> {}",self.ip, self.port, status);
     }
 }
--- learn_by_projects/firewall_rule/src/main.rs
+++ learn_by_projects/firewall_rule/src/main.rs
@@ -1,14 +1,14 @@
-use firewall_rule::FireWallRule;
+use firewall_rule::{Packet, FireWallRule};
 
 fn main(){
     let packet= Packet{
-        src_ip= "192.168.1.10".to_string(),
-        dst_ip="10.0.0.5".to_string(),
+        src_ip: "192.168.1.10".to_string(),
+        dst_ip:"10.0.0.5".to_string(),
         port: 22,
     };
     
-    let rule1 = FirewallRule::new("192.168.1.10", 22, false); // block
-    let rule2 = FirewallRule::new("192.168.1.20", 80, true); //allow
+    let rule1 = FireWallRule::new("192.168.1.10", 22, false); // block
+    let rule2 = FireWallRule::new("192.168.1.20", 80, true); //allow
 
     rule1.display();
     rule2.display();
